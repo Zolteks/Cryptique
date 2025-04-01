f4b606e27e2a083c08f6decbc64ef475075f5099
Léo <leoclipet26@gmail.com>
2025-04-01 11:09:28 +0200

    Update OBJ_Collectable.cs

--- Cryptique/Assets/Script/OBJ_Collectable.cs
+++ Cryptique/Assets/Script/OBJ_Collectable.cs
@@ -1,30 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class OBJ_Collectable : MonoBehaviour, IInteractable
 {
     /* Variables */
     public bool bCanInteract { get; protected set; } = true;
+    [SerializeField] private GameObject m_objInteractable;
 
     /* Functions */
     public bool Interact()
     {
-        if (bCanInteract)
-        {
-            // Add to inventory
-            return true;
-        }
-        return false;
+        if (!bCanInteract)
+            return false;
+
+        // Add to inventory
+        Debug.Log("Collected " + gameObject.name);
+        Destroy(gameObject);
+        return true;
     }
 
-    public void UseOn()
+    public void UseOn(GameObject ObjectToInteract)
     {
-        // Use item on object
-    }
+        // Use item on OBJ_Interactable
+        if (!bCanInteract)
+            return;
 
-    public void Drag()
-    {
+        if (ObjectToInteract != m_objInteractable.gameObject)
+            return;
+
+        // Use item on ObjectToInteract
+        if (!ObjectToInteract.TryGetComponent(out IInteractable interactable))
+            return;
         
+        if (!interactable.bCanInteract)
+            return;
+
+        Debug.Log("Used " + gameObject.name + " on " + ObjectToInteract.name);
+        interactable.Interact();
+        Destroy(gameObject);
     }
 }
