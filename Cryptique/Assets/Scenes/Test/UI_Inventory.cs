94a72eff0297ec2841d2ca60f3d404146877e3f3
Ludovic28 <148745476+Ludovic28@users.noreply.github.com>
2025-04-01 18:07:38 +0200

    UI good manque juste la partie creation des slot

--- Cryptique/Assets/Scenes/Test/UI_Inventory.cs
+++ Cryptique/Assets/Scenes/Test/UI_Inventory.cs
@@ -2,18 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEditor.Progress;
+
+
 
 public class UI_Inventory : MonoBehaviour
 {
 
     public GameObject gInventoryPanel;
-    public GameObject gInventoryColumnPanel;
     public Button bInventoryButton;
 
     // Start is called before the first frame update
     void Start()
     {
         gInventoryPanel.SetActive(false);
+
     }
 
     // Update is called once per frame
---
+++ Cryptique/Assets/Scenes/Test/UI_ManageInventory.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_ManageInventory : MonoBehaviour
+{
+
+    private int iInventorySlots = 20;
+
+    public GameObject gInventoryPrefab;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
