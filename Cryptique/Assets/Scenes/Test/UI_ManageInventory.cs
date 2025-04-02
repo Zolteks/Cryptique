c531f20a85d856c9ebb0e08dc384b32a6622a8fb
Ludovic28 <148745476+Ludovic28@users.noreply.github.com>
2025-04-02 10:13:40 +0200

    UI & List des pos fini

--- Cryptique/Assets/Scenes/Test/UI_ManageInventory.cs
+++ Cryptique/Assets/Scenes/Test/UI_ManageInventory.cs
@@ -4,21 +4,38 @@ using UnityEngine;
 
 public class UI_ManageInventory : MonoBehaviour
 {
-
-    private int iInventorySlots = 20;
-
-    public GameObject gInventoryPrefab;
+    public GameObject gContentPanel;
     
+    private List<Vector3> lPositionGrid = new List<Vector3>();
+    private List<string> lNameGrid = new List<string>();
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        // Forcer la mise à jour immédiate de tous les Canvas et leurs LayoutGroups
+        Canvas.ForceUpdateCanvases();
+
+        // Récupérer tous les RectTransform des enfants
+        RectTransform[] allChildren = gContentPanel.GetComponentsInChildren<RectTransform>();
+
+        for (int i = 1; i < allChildren.Length; i += 2)
+        {
+            RectTransform child = allChildren[i];
+
+            lPositionGrid.Add(child.anchoredPosition);
+            lNameGrid.Add(child.name);
+
+        }
+
+        print(lPositionGrid);
+        print(lNameGrid);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < lPositionGrid.Count; i++)
+        {
+            print(lNameGrid[i] + " : " + lPositionGrid[i]);
+        }
     }
 }
