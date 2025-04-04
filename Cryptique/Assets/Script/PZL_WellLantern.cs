1beeca27580c0447a158975aa6e01b0f1642747a
SummerRainyAppleHugo <hugo.h.xia@gmail.com>
2025-04-04 14:57:11 +0200

    Finished Lantern (Need to add generic puzzle + OBJ_Interactable)

--- Cryptique/Assets/Script/PZL_WellLantern.cs
+++ Cryptique/Assets/Script/PZL_WellLantern.cs
@@ -8,31 +8,28 @@ using UnityEngine.EventSystems;
 public class PZL_WellLantern : MonoBehaviour, IDragHandler, IPointerDownHandler
 {
     [SerializeField] private float rotationSpeed = 5f;
-    private float previousAngle;
-    private bool isDragging = false;
+    [SerializeField] private GameObject lanterne;
+    [SerializeField] private float maxLeftRotation = -1800f;
+    [SerializeField] private float maxRightRotation = 720f;
+
+    [SerializeField] private float minScale = 0.5f;
+    [SerializeField] private float maxScale = 2f;
+
+    private float currentRotationZ;
     private Vector2 centerPoint;
+    private bool isDragging;
+    private Vector3 initialScale;
 
-    [SerializeField] private GameObject lantern;
-
-    private void Update()
+    private void Start()
     {
-        if (Input.GetMouseButtonUp(0))
-        {
-            isDragging = false;
-        }
-    }
-
-    private float GetAngle(Vector2 screenPosition)
-    {
-        Vector2 direction = screenPosition - centerPoint;
-        return Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        currentRotationZ = transform.eulerAngles.z;
+        initialScale = lanterne.transform.localScale;
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         isDragging = true;
         centerPoint = RectTransformUtility.WorldToScreenPoint(null, transform.position);
-        previousAngle = GetAngle(eventData.position);
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -40,24 +37,33 @@ public class PZL_WellLantern : MonoBehaviour, IDragHandler, IPointerDownHandler
         if (!isDragging) return;
 
         Vector2 direction = eventData.position - centerPoint;
+        float targetAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        // DeadZone du Drag
-        if (direction.magnitude < 20f) return;
+        float deltaRotation = Mathf.DeltaAngle(currentRotationZ, targetAngle);
+        currentRotationZ = Mathf.Clamp(currentRotationZ + deltaRotation * rotationSpeed * Time.deltaTime,
+                                      maxLeftRotation, maxRightRotation);
 
-        float currentAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        float deltaAngle = Mathf.DeltaAngle(transform.eulerAngles.z, currentAngle);
-
-        transform.rotation = Quaternion.Euler(0, 0, currentAngle);
+        transform.rotation = Quaternion.Euler(0, 0, currentRotationZ);
+        UpdateLanterneScale();
     }
 
-    public void DragLimit()
+    private void UpdateLanterneScale()
     {
+        // Since camera placed in front it seems left and right are inverted.
+        // Scaling Left is scaled up x2 and Right is scaled down /2
+        float normalizedRotation = Mathf.InverseLerp(maxLeftRotation, maxRightRotation, currentRotationZ);
 
+        float currentScale = Mathf.Lerp(maxScale, minScale, normalizedRotation);
+
+        lanterne.transform.localScale = initialScale * currentScale;
     }
 
-    public void WindItUp()
+    private void Update()
     {
-
+        if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+        }
     }
 
 }
