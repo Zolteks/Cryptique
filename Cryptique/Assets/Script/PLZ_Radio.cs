e6d4036e4471f24afbca9861bce7d633e8dc38bd
Hexan <96941840+Zolteks@users.noreply.github.com>
2025-04-07 16:32:34 +0200

    Outline shader + Radio puzzle WIP

--- Cryptique/Assets/Script/PLZ_Radio.cs
+++ Cryptique/Assets/Script/PLZ_Radio.cs
@@ -9,23 +9,42 @@ public class PLZ_Radio : Puzzle
     [SerializeField] GameObject buttonH;
     Material shader;
 
-    float curFreqW;
-    float curFreqH;
+    public float curFreqW;
+    public float curFreqH;
 
     float targetFreqW;
     float targetFreqH;
 
+    [SerializeField] float toleranceW = .1f;
+    [SerializeField] float toleranceH = .05f;
+
+    float timeForValidation = 5f;
+
     void Start()
     {
         shader = frequenceDisplay.materials[0];
+        curFreqW = 2;
+        curFreqH = .1f;
+
+        targetFreqW = 5f;
+        targetFreqH = .15f;
+        shader.SetFloat("_TargetFreqWidth", targetFreqW);
+        shader.SetFloat("_TargetFreqHeight", targetFreqH);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ()
-        {
+        // Set the shader values base on the current frequence.
+        shader.SetFloat("_FreqWidth", curFreqW);
+        shader.SetFloat("_FreqHeight", curFreqH);
 
+        // Checks if the current frequence is between acceptable bounds for Width and Height
+        if ((curFreqH < targetFreqH + toleranceH && curFreqH > targetFreqH - toleranceH) && (curFreqW < targetFreqW + toleranceW && curFreqW > targetFreqW - toleranceW))
+        {
+            timeForValidation -= Time.deltaTime;
+            if (timeForValidation < 0)
+                Complete();
         }
     }
 }
