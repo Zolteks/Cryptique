78833ab26abe513ccd25827f4de22a9c540d9b0a
Ludovic28 <148745476+Ludovic28@users.noreply.github.com>
2025-04-08 16:57:02 +0200

    add du Complete()

--- Cryptique/Assets/Script/Puzzles/PZL_Chain.cs
+++ Cryptique/Assets/Script/Puzzles/PZL_Chain.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class PZL_Chain : OBJ_InteractOnDrop
 {
+    [SerializeField] private PZL_ChainComplete PZL_ChainComplete;
+
     public override bool Interact()
     {
         Destroy(gameObject);
+        PZL_ChainComplete.CompleteChainPuzzle();
         return true;
     }
 }
