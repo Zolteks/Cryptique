3188d536b3204fa421af6c78782f77fc343ec573
Léo <leoclipet26@gmail.com>
2025-04-09 16:36:40 +0200

    Interaction 3D blocked when UI in front

--- Cryptique/Assets/Script/InteractManager.cs
+++ Cryptique/Assets/Script/InteractManager.cs
@@ -1,15 +1,28 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class InteractManager : Singleton<InteractManager>
 {
     private static InputManager m_inputManager;
     
     private Camera m_camera;
+    [SerializeField]
+    private Canvas m_canvas;
+    [SerializeField]
+    private GraphicRaycaster m_graphicRaycaster;
     
     private void Awake()
     {
         m_inputManager = InputManager.Instance;
         m_camera = Camera.main;
+        foreach (var objectInScene in SceneManager.GetActiveScene().GetRootGameObjects())
+        {
+            if (objectInScene.transform as RectTransform == null)
+                continue;
+            m_canvas = objectInScene.transform.GetComponent<Canvas>();
+            m_graphicRaycaster = m_canvas.GetComponent<GraphicRaycaster>();
+        }
     }
 
     private void OnEnable()
@@ -24,6 +37,10 @@ public class InteractManager : Singleton<InteractManager>
 
     private void OnInteract(Vector2 pos, float time)
     {
+        // Raycast UI
+        if (m_graphicRaycaster != null && Utils.DetectHitWithUI(pos, m_graphicRaycaster))
+            return;
+        // Raycast 3D
         GameObject hitObject = Utils.GetObjectUnderTouch(m_camera, pos);
         if (hitObject != null)
         {
