f146eb93d20235902502cb50ca322fe00880bb55
Ludovic28 <148745476+Ludovic28@users.noreply.github.com>
2025-04-23 12:11:58 +0200

    fixes interaction + cam gutter + Ui disable

--- Cryptique/Assets/Script/OBJ_DropOnHorse.cs
+++ Cryptique/Assets/Script/OBJ_DropOnHorse.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class OBJ_DropOnHorse : OBJ_InteractOnDrop
 {
+    private int counterOBj = 0;
     public override bool Interact()
     {
-        GetComponent<PZL_Horse>().DoComplete();
+        counterOBj++;
+
+        if (counterOBj == 2) GetComponent<PZL_Horse>().DoComplete();
 
         return true;
     }
