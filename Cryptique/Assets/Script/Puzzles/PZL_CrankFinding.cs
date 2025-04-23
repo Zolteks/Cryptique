5d9e382e0b1a04738c455636784916bb53f93b1c
Ludovic28 <148745476+Ludovic28@users.noreply.github.com>
2025-04-23 11:35:05 +0200

    fixe interaction village

--- Cryptique/Assets/Script/Puzzles/PZL_CrankFinding.cs
+++ Cryptique/Assets/Script/Puzzles/PZL_CrankFinding.cs
@@ -7,11 +7,20 @@ public class PZL_CrankFinding : Puzzle
     [SerializeField] private OBJ_Item m_Crank;
     [SerializeField] private GameObject m_StableIndoor;
 
+    private GameObject m_UIplayGameObject;
+
+    private void Start()
+    {
+        m_UIplayGameObject = GameObject.Find("UIPlay");
+        m_UIplayGameObject.SetActive(false);
+    }
+
     public void TakeCrank()
     {
         SGL_InventoryManager.Instance.AddItem(m_Crank);
         Destroy(m_StableIndoor);
 
+        m_UIplayGameObject.SetActive(true);
         Complete();
     }
 }
