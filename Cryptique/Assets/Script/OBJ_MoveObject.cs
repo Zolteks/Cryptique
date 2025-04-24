3b9d6ff0ec7268d25bbfc6a8ed773ab7f08ca54a
JoshuaLetessier <147531448+JoshuaLetessier@users.noreply.github.com>
2025-04-24 16:39:00 +0200

    pzl tsuki

---
+++ Cryptique/Assets/Script/OBJ_MoveObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class OBJ_MoveObject : OBJ_Interactable
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private GameObject targetPosition;
+    private bool isMoving = false;
+    private void Update()
+    {
+        if (isMoving)
+        {
+            MoveObject();
+        }
+    }
+    public override bool Interact()
+    {
+        isMoving = !isMoving;
+        gameObject.GetComponent<BoxCollider>().enabled = false;
+        return true;
+    }
+
+    private void MoveObject()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition.transform.position, moveSpeed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, targetPosition.transform.position) < 0.1f)
+        {
+            isMoving = false;
+        }
+    }
+}
+
---
+++ Cryptique/Assets/Script/OBJ_VisualisableObject.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class OBJ_VisualisableObject : OBJ_Interactable
+{
+
+    [SerializeField] private GameObject uiVisualisation;
+    [SerializeField] private OBJ_Item item;
+
+    public override bool Interact()
+    {
+        uiVisualisation.SetActive(!uiVisualisation.activeSelf);
+        if (uiVisualisation.activeSelf)
+        {
+            UI_ObjectDescription uiObjectDescription = uiVisualisation.GetComponent<UI_ObjectDescription>();
+            if (uiObjectDescription != null)
+            {
+                uiObjectDescription.Show(item);
+            }
+        }
+        else
+        {
+            Destroy(uiVisualisation);
+        }
+
+        return true;
+    }
+    public override void TriggerInteract()
+    {
+        base.TriggerInteract();
+    }
+}
+
