7e682c2059581b4e76a803ae1f737b15fc6f4bc4
Ludovic28 <148745476+Ludovic28@users.noreply.github.com>
2025-04-26 15:08:54 +0200

    message pour expliquer le drag and drop

---
+++ Cryptique/Assets/Script/LaunchInformation.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+
+public class LaunchInformation : OBJ_Collectable
+{
+    [SerializeField] private GameObject m_UIInfo;
+
+    public override bool Interact()
+    {
+        if (!CanInteract())
+            return false;
+
+        StartCoroutine(CoroutineLaunchInfo(2f));
+        
+        return true;
+    }
+
+    private IEnumerator CoroutineLaunchInfo(float delay)
+    {
+        m_UIInfo.SetActive(true);
+        PC_PlayerController.Instance.DisableInput();
+
+        yield return new WaitForSeconds(delay);
+
+        // Add to inventory
+        SGL_InventoryManager.Instance.AddItem(m_item);
+
+        // Notify the GameProgressionManager that an item as been collected
+        GameProgressionManager.Instance.CollectItem(m_item.GetRegion(), m_item.GetName());
+        Debug.Log($"Item {m_item.name} collected ! add to GameProgressionManager ");
+
+        m_UIInfo.SetActive(false);
+        Destroy(gameObject);
+        PC_PlayerController.Instance.EnableInput();
+    }
+}
